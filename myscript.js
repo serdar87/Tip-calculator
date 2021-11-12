6a88bfd9ad0f4bec53eec52863c66b3da7a9bd11
serdar Tümay <83467033+serdar87@users.noreply.github.com>
2021-11-12 21:11:47 +0100

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,64 +1,59 @@
-let resulttip = 0;
-let resultperson = 0;
+const btn = document.querySelectorAll(".button1");
 let billamount = 0;
 let personnumber = 0;
-let tipamount = 0;
-let totalamount = 0;
-let btn = document.querySelectorAll(".button1");
-let tipSelected=0;
-
+let tipSelected = 0;
 
 function calculate() {
-    if (personnumber == 0 || personnumber === undefined) {
-        return false;
-    } else {
-        totalamount=((billamount*tipSelected/100)+billamount);
-        resulttip=((billamount*tipSelected/100)/personnumber).toFixed(2);
-        resultperson = (totalamount/personnumber).toFixed(2);
-        document.getElementById('tipsum').innerHTML = resulttip;
-        document.getElementById('personsum').innerHTML =resultperson ;
-    }
+  if (personnumber == 0 || personnumber === undefined) {
+    return false;
+  } else {
+    let totalamount = (billamount * tipSelected) / 100 + billamount;
+    let resulttip = ((billamount * tipSelected) / 100 / personnumber).toFixed(
+      2
+    );
+    let resultperson = (totalamount / personnumber).toFixed(2);
+    document.getElementById("tipsum").innerHTML = resulttip;
+    document.getElementById("personsum").innerHTML = resultperson;
+  }
 }
 
-function calculate1() {
-    billamount = Number(document.getElementById("bill").value);
-    
-    calculate();
-};
+function calcbillinput() {
+  billamount = Number(document.getElementById("bill").value);
 
-function calculate2(event) {
+  calculate();
+}
 
-    tipSelected = Number(event.target.value); 
-    calculate();
-};
+function calcbutton(event) {
+  tipSelected = Number(event.target.value);
+  calculate();
+}
 
-function calculate3() {
-    personnumber = Number(document.getElementById("personinput").value);
-    calculate();
-    
+function calcpeople() {
+  personnumber = Number(document.getElementById("personinput").value);
+  calculate();
 }
 
 for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", function () {
+    removeActiveClass();
+    btn[i].classList.add("active");
+  });
+}
 
-    btn[i].addEventListener("click", function () {
-        remove();
-        btn[i].classList.add("active")
-    });
-};
-
-function remove() {
-    for (let i = 0; i < btn.length; i++) {
-        btn[i].classList.remove("active")
-    }
+function removeActiveClass() {
+  for (let i = 0; i < btn.length; i++) {
+    btn[i].classList.remove("active");
+  }
 }
 
 function reset() {
-    remove();
-    document.getElementById('tipsum').innerHTML = "$" + "0.00";
-    document.getElementById('personsum').innerHTML = "$" + "0.00";
-    document.getElementById('bill').value="";
-    document.getElementById('personinput').value="";
-    document.getElementById('custominput').value="";
-
-    
+  removeActiveClass();
+  document.getElementById("tipsum").innerHTML = "$" + "0.00";
+  document.getElementById("personsum").innerHTML = "$" + "0.00";
+  document.getElementById("bill").value = "";
+  document.getElementById("personinput").value = "";
+  document.getElementById("custominput").value = "";
+  billamount = 0;
+  personnumber = 0;
+  tipSelected = 0;
 }
